209f3940fd3a3bab5f093b492b20e99325a3a417
Artur Simoni <artursimonijesus@gmail.com>
2025-05-08 10:39:11 -0300

    FIX: fazendo o codigo funcionar

--- udp/client.py
+++ udp/client.py
@@ -1,22 +1,23 @@
-import socket 
+import socket
 
-def send_message(message:str, host: str, port:int):
-    server = socket.socket(socket.AF_INET, socket.SOCK.DGRAM)
-    server.settimeout(1)
+def send_message(host: str, port: int):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.settimeout(1) 
 
     while True:
-        message = input('type your message')
-        server.sendto(message.encode(),(HOST, PORT))
+        message = input('Digite sua mensagem: ')
+        
+        client_socket.sendto(message.encode(), (host, port))
 
-        data, addr = server.recvfrom(1024)
-        print(f'[Message]: {data.decode()}')
+        try:
 
+            data, addr = client_socket.recvfrom(1024)
+            print(f'[Mensagem recebida]: {data.decode()}')
+        except socket.timeout:
+            print("[Erro]: Tempo limite atingido. Nenhuma resposta do servidor.")
 
-if __name__=='_main__':
-
+if __name__ == '__main__':
     HOST = 'localhost'
     PORT = 8000
 
-    
     send_message(HOST, PORT)
-   
---
+++ udp/server.py
@@ -0,0 +1,27 @@
+import socket
+
+def start_server(host: str, port: int):
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind((host, port))
+
+    clients = set()
+    
+    print(f'Servidor iniciado em {host}:{port}\nEscutando agora...')
+
+    while True:
+        data, addr = server.recvfrom(1024)
+        print(f'[CLIENTE] {addr}: {data.decode()}')
+
+        if addr not in clients:
+            clients.add(addr)
+            print(f'Novo cliente conectado: {addr}')
+
+        for client_addr in clients:
+            server.sendto(data, client_addr)
+            print(f'[SERVIDOR] Enviado para {client_addr}: {data.decode()}')
+
+if __name__ == '__main__':
+    HOST = 'localhost'
+    PORT = 8000
+
+    start_server(HOST, PORT)
